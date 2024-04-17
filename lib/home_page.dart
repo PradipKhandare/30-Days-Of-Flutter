de378217ff6fa1f6a17671236894c6bd52f99ff9
PradipKhandare <pradipkhandare111@gmail.com>
2024-04-17 13:06:51 +0530

    data_tpes, scaffold, appbar, body, widgets

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    String name = 'Pradip';
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text('Pradips Tech'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("welcome to the $days days of flutter by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
