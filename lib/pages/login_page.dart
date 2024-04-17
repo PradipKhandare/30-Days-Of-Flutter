8bfe4fa7ef1becaee9b35ea2c80895c178251094
PradipKhandare <pradip.khandare@neutrinotechlabs.com>
2024-04-17 19:10:01 +0530

    Add animation to login screen and components

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,66 +1,110 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:thirty_days_of_flutter/Utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
+    _controller.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
+    String name = "";
     return Material(
-        color: Colors.white,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(height: 50),
-              Image.asset(
+      color: Colors.white,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 50),
+            FadeTransition(
+              opacity: _animation,
+              child: Image.asset(
                 "assets/images/loginimage.png",
                 fit: BoxFit.cover,
               ),
-              const SizedBox(height: 20),
-              const Text(
-                "Welcome in Pradip's Tech",
+            ),
+            const SizedBox(height: 20),
+            FadeTransition(
+              opacity: _animation,
+              child: const Text(
+                "Welcome to Pradip's Tech",
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
-              const SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                    vertical: 16.0, horizontal: 32.0),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: const InputDecoration(
-                          hintText: "Enter Your Username",
-                          labelText: "Username"),
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                          hintText: "Enter Your Password",
-                          labelText: "Password"),
-                    ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        hintText: "Enter Your Username", labelText: "Username"),
+                    onChanged: (value) {
+                      name = value;
+                    },
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        hintText: "Enter Your Password", labelText: "Password"),
+                  ),
+                  const SizedBox(height: 20),
+                  ScaleTransition(
+                    scale: _animation,
+                    child: ElevatedButton(
                       onPressed: () {
                         Navigator.pushNamed(context, MyRoutes.homeRoute);
                       },
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(255, 228, 106,
-                            45), // Change the background color here
+                        backgroundColor:
+                            const Color.fromARGB(255, 228, 106, 45),
                         minimumSize: const Size(200, 40),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              8), // Change the radius here
+                        ),
+                        shadowColor: Colors.blue,
                       ),
-                      child: const Text('Login',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          )),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-        ));
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
