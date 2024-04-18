494daaa9e120a5a96735f1f2f888357d9f756b7e
PradipKhandare <pradip.khandare@neutrinotechlabs.com>
2024-04-18 09:25:36 +0530

    add_UI_Validations

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -24,79 +24,112 @@ class _LoginPageState extends State<LoginPage>
     _controller.forward();
   }
 
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    final currentState = _formKey.currentState;
+    if (currentState?.validate() ?? true) {
+      await Navigator.pushNamed(context, MyRoutes.homeRoute);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     String name = "";
     return Material(
       color: Colors.white,
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            const SizedBox(height: 50),
-            FadeTransition(
-              opacity: _animation,
-              child: Image.asset(
-                "assets/images/loginimage.png",
-                fit: BoxFit.cover,
+        child: Form(
+          child: Column(
+            children: [
+              const SizedBox(height: 50),
+              FadeTransition(
+                opacity: _animation,
+                child: Image.asset(
+                  "assets/images/loginimage.png",
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-            FadeTransition(
-              opacity: _animation,
-              child: const Text(
-                "Welcome to Pradip's Tech",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              const SizedBox(height: 20),
+              FadeTransition(
+                opacity: _animation,
+                child: const Text(
+                  "Welcome to Pradip's Tech",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(
-                        hintText: "Enter Your Username", labelText: "Username"),
-                    onChanged: (value) {
-                      name = value;
-                    },
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                        hintText: "Enter Your Password", labelText: "Password"),
-                  ),
-                  const SizedBox(height: 20),
-                  ScaleTransition(
-                    scale: _animation,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 32.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          hintText: "Enter Your Username",
+                          labelText: "Username"),
+                      validator: (value) {
+                        if (value?.isEmpty ?? false) {
+                          return 'Please username can not be empty';
+                        }
+                        return null;
                       },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor:
-                            const Color.fromARGB(255, 228, 106, 45),
-                        minimumSize: const Size(200, 40),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(
-                              8), // Change the radius here
+
+                      onChanged: (value) {
+                        name = value;
+                      },
+                      autovalidateMode: AutovalidateMode
+                          .onUserInteraction, // Enables auto-validation
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                          hintText: "Enter Your Password",
+                          labelText: "Password"),
+                      validator: (value) {
+                        if ((value?.isEmpty ?? false) == true) {
+                          return 'Please password can not be empty';
+                        } else if ((value?.length ?? 0) < 6) {
+                          return 'password minimum length should be 6';
+                        }
+                        return null;
+                      },
+
+                      autovalidateMode: AutovalidateMode
+                          .onUserInteraction, // Enables auto-validation
+                    ),
+                    const SizedBox(height: 20),
+                    ScaleTransition(
+                      scale: _animation,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          moveToHome(context);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 228, 106, 45),
+                          minimumSize: const Size(200, 40),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                                8), // Change the radius here
+                          ),
+                          shadowColor: Colors.blue,
                         ),
-                        shadowColor: Colors.blue,
-                      ),
-                      child: const Text(
-                        'Login',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
+                        child: const Text(
+                          'Login',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            )
-          ],
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
