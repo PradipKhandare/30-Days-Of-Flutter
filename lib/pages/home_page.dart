53bbc1c4dec81412cd797775db96114680fb7df1
PradipKhandare <pradip.khandare@neutrinotechlabs.com>
2024-04-22 12:36:15 +0530

    local files | load & decode json

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,37 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:thirty_days_of_flutter/models/catalog.dart';
 import 'package:thirty_days_of_flutter/widgets/drawer.dart';
 import 'package:thirty_days_of_flutter/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int days = 30;
+  String name = 'Pradip';
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    print(productsData);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    int days = 30;
-    String name = 'Pradip';
     final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
     return Scaffold(
         appBar: AppBar(
